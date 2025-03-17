195c0c5c2b9436b319e1bd7e6aa51aefb3c077f4
thong <thongviet@gmail.com>
2025-03-17 16:22:22 +0700

    update next-auth login

---
+++ src/app/(frontend)/(auth)/next-auth.ts
@@ -0,0 +1,58 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const res = await fetch('http://localhost:3000/api/auth/login', {
+          method: 'POST',
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password,
+          }),
+          headers: { 'Content-Type': 'application/json' },
+        })
+
+        const user = await res.json()
+
+        if (res.ok && user) {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+          }
+        } else {
+          throw new Error(user.message)
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+      }
+      return session
+    },
+  },
+  pages: {
+    signIn: '/auth/login',
+  },
+}
+
+export default NextAuth(authOptions)
